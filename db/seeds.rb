98ce69ac57d75d8a212f0de4408264216cb6ee99
sadattachi <nazarii200322@gmail.com>
2022-06-30 14:56:22 +0300

    Changed one worker to be unactive

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 workers = Worker.create([{ last_name: 'Tester', first_name: 'Nazar', age: 18, role: 'Developer', active: true },
                          { last_name: 'Lazy', first_name: 'Ivan', age: 20, role: 'Manager', active: true },
                          { last_name: 'Fast', first_name: 'John', age: 18, role: 'UI/UX Designer', active: true },
-                         { last_name: 'Fired', first_name: 'Ben', age: 18, role: 'Developer', active: true }])
+                         { last_name: 'Fired', first_name: 'Ben', age: 18, role: 'Developer', active: false }])
 
 Ticket.create([{ title: 'test', description: 'test', worker: workers[0], state: 'Pending' },
                { title: 'more', description: 'test', worker: workers[1], state: 'In progress' },
