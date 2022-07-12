4976724e53dba4d9c04e189aa2e32369e2bfc4b4
sadattachi <nazarii200322@gmail.com>
2022-07-12 17:57:11 +0300

    Refactor worker model tests

--- spec/models/worker_spec.rb
+++ spec/models/worker_spec.rb
@@ -1,39 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Worker, type: :model do
-  let(:worker) do
-    Worker.new(last_name: 'test',
-               first_name: 'test',
-               age: 40,
-               role: 'Developer')
+  let(:worker) { FactoryBot.create :worker }
+
+  context 'when valid attributes' do
+    it { expect(worker).to be_valid }
   end
 
-  it 'is valid with valid attributes' do
-    expect(worker).to be_valid
+  context 'when last_name is longer than 20 characters' do
+    before { worker.last_name = 'way too long last name for worker' }
+    it { expect(worker).to_not be_valid }
   end
 
-  it 'is not valid with last_name longer than 20 characters' do
-    worker.last_name = 'way too long last name for worker'
-    expect(worker).to_not be_valid
+  context 'when first_name is longer than 20 characters' do
+    before { worker.first_name = 'way too long first name for worker' }
+    it { expect(worker).to_not be_valid }
   end
 
-  it 'is not valid with first_name longer than 20 characters' do
-    worker.first_name = 'way too long first name for worker'
-    expect(worker).to_not be_valid
+  context 'when age is below 16' do
+    before { worker.age = 10 }
+    it { expect(worker).to_not be_valid }
   end
 
-  it 'is not valid with age below 16' do
-    worker.age = 10
-    expect(worker).to_not be_valid
+  context 'when age is above 60' do
+    before { worker.age = 70 }
+    it { expect(worker).to_not be_valid }
   end
 
-  it 'is not valid with age above 60' do
-    worker.age = 70
-    expect(worker).to_not be_valid
-  end
-
-  it 'is not valid if role is not Developer, Manager or UI/UX Designer' do
-    worker.role = 'test'
-    expect(worker).to_not be_valid
+  context 'when role is not valid' do
+    before { worker.role = 'test' }
+    it { expect(worker).to_not be_valid }
   end
 end
