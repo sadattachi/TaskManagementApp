29a07469a1aeda08439e5aa191117020dbdc3ea1
sadattachi <nazarii200322@gmail.com>
2022-07-12 15:56:26 +0300

    Remove unnecessary comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 workers = Worker.create([{ last_name: 'Tester', first_name: 'Nazar', age: 18, role: 'Developer', active: true },
                          { last_name: 'Lazy', first_name: 'Ivan', age: 20, role: 'Manager', active: true },
                          { last_name: 'Fast', first_name: 'John', age: 18, role: 'UI/UX Designer', active: true },
