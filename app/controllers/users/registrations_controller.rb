9876e82fe3342094e114e790c3818b51e8ab92f4
sadattachi <nazarii200322@gmail.com>
2022-07-25 17:41:15 +0300

    Add worker creation to user registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
+  before_action :create_worker, only: :create
+  after_action :add_worker_id, only: :create
 
   private
+
+  def create_worker
+    @worker = Worker.new(worker_params)
+    render json: @worker.errors, status: :unprocessable_entity unless @worker.save
+  end
+
+  def add_worker_id
+    if @user.persisted?
+      @user.update(worker_id: @worker.id)
+    else
+      @worker.destroy
+    end
+  end
 
   def respond_with(resource, _opts = {})
     resource.persisted? ? register_success : register_failed
@@ -12,6 +27,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def register_failed
-    render json: { message: 'Signed up failure.' }
+    render json: { message: @user.errors.full_messages }
+  end
+
+  def worker_params
+    params.require(:worker).permit(:last_name, :first_name, :age, :role, :active)
   end
 end
