82a7b3c538abf696b41b7179adaa67e038598530
sadattachi <nazarii200322@gmail.com>
2022-07-25 17:40:38 +0300

    Permit is_admin parameter for user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token, if: :json_request?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[is_admin])
+  end
 
   def json_request?
     request.format.json?
