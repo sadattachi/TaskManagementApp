3281de7d6be0c1493430d2f8142ec4b77b79863e
sadattachi <nazarii200322@gmail.com>
2022-07-25 16:35:47 +0300

    Add creator_worker_id to tickets table

---
+++ db/migrate/20220725132204_add_creator_worker_id_to_tickets_table.rb
@@ -0,0 +1,5 @@
+class AddCreatorWorkerIdToTicketsTable < ActiveRecord::Migration[6.1]
+  def change
+    add_column :tickets, :creator_worker_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_131429) do
+ActiveRecord::Schema.define(version: 2022_07_25_132204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_07_25_131429) do
     t.string "state"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_worker_id"
   end
 
   create_table "users", force: :cascade do |t|
