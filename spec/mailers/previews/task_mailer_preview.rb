08b42512e25632a5e924e4b7cccada135359e8c8
sadattachi <nazarii200322@gmail.com>
2022-08-09 14:58:32 +0300

    Add previews

--- spec/mailers/previews/task_mailer_preview.rb
+++ spec/mailers/previews/task_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/task_mailer
 class TaskMailerPreview < ActionMailer::Preview
+  def new_task_email
+    @user = User.new(email: 'test@example.com', password: 'password')
+    @ticket = Ticket.new(title: 'preview', description: 'preview', state: 'Done', worker_id: 1)
 
+    TaskMailer.with(user: @user, ticket: @ticket).new_task_email
+  end
 end
