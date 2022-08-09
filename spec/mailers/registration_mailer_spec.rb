f1af0e4919122fe97b10054c704d6f0204fe1b8a
sadattachi <nazarii200322@gmail.com>
2022-08-09 15:45:09 +0300

    Add specs for registration emails

--- spec/mailers/registration_mailer_spec.rb
+++ spec/mailers/registration_mailer_spec.rb
@@ -1,5 +1,19 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe RegistrationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'welcome' do
+    let(:mail) { RegistrationMailer.with(user: User.first).welcome_email }
+
+    it 'renders the headers' do
+      puts mail.body.encoded
+      expect(mail.subject).to eq('Welcome to Task Management App!')
+      expect(mail.to).to eq(['nazar@gmail.com'])
+      expect(mail.from).to eq(['test@example.com'])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('Welcome, Nazar Tester')
+      expect(mail.body.encoded).to match('Best of luck!')
+    end
+  end
 end
