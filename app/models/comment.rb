385e6c54065314e97eb58e313f9eec26b1b47431
sadattachi <nazarii200322@gmail.com>
2022-08-18 19:59:17 +0300

    Add worker model relation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :ticket
+  belongs_to :worker
 
   belongs_to :reply_to, class_name: 'Comment', foreign_key: 'reply_to_comment_id', optional: true
   has_many :replies, class_name: 'Comment', foreign_key: 'reply_to_comment_id'
