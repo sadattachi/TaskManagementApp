368ab1c43e491c273d1c09ddc86ee0b04d609fb2
sadattachi <nazarii200322@gmail.com>
2022-08-18 19:36:13 +0300

    Add show action and views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,16 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[show update destroy]
   def index
     @comments = Comment.where(ticket_id: params[:ticket_id].to_i)
+  end
+
+  def show; end
+
+  private
+
+  def set_comment
+    @comment = Comment.where(ticket_id: params[:ticket_id].to_i).find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: e.message }, status: :not_found
   end
 end
