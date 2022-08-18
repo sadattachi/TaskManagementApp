ea1e862c5fdc9f9c1dee6c4479ad338fd4ad4271
sadattachi <nazarii200322@gmail.com>
2022-08-18 18:07:30 +0300

    Add comments table migration

---
+++ db/migrate/20220818091622_add_comments_table.rb
@@ -0,0 +1,11 @@
+class AddCommentsTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.integer :worker_id
+      t.integer :ticket_id
+      t.string :message
+      t.integer :reply_to_comment_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_132204) do
+ActiveRecord::Schema.define(version: 2022_08_18_091622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "worker_id"
+    t.integer "ticket_id"
+    t.string "message"
+    t.integer "reply_to_comment_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
