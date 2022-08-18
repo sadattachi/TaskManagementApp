b52d8b99aab0ea5e445e87a33ab18821269066f2
sadattachi <nazarii200322@gmail.com>
2022-08-18 20:58:41 +0300

    Add update action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[show update destroy]
   def index
-    @comments = Comment.where(ticket_id: params[:ticket_id].to_i)
+    @comments = Comment.where(ticket_id: params[:ticket_id].to_i).order(:created_at)
   end
 
   def show; end
@@ -16,6 +16,18 @@ class CommentsController < ApplicationController
       render :show, status: :created, location: @ticket_comment
     else
       render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @comment.worker_id == current_user.id
+      if @comment.update(message: params[:comment][:message])
+        render :show, status: :ok, location: @ticket_comment
+      else
+        render json: @comment.errors, status: :unprocessable_entity
+      end
+    else
+      forbidden_message('Only author can edit this comment!')
     end
   end
 
