db894c6bfa51761bf41b5c6c04b938200ad163c1
sadattachi <nazarii200322@gmail.com>
2022-08-19 14:25:22 +0300

    Add specs for comments without auth

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
+require 'devise/jwt/test_helpers'
 
-RSpec.describe "Comments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Comments', type: :request do
+  context 'with valid authentication' do
+  end
+  context 'without authentication' do
+    context 'GET /tickets/1/comments' do
+      before { get '/tickets/1/comments', as: :json }
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).to render_template(:index) }
+    end
+
+    context 'GET /tickets/1/comments/1' do
+      before { get '/tickets/1/comments/1', as: :json }
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response).to render_template(:show) }
+    end
+
+    context 'POST /tickets/1/comments' do
+      before { post '/tickets/1/comments', as: :json }
+
+      it { expect(response).to have_http_status(:unauthorized) }
+      it { expect(response.parsed_body['error']).to eq('You need to log in to continue!') }
+    end
+
+    context 'PUT /tickets/1/comments' do
+      before { put '/tickets/1/comments/1', as: :json }
+
+      it { expect(response).to have_http_status(:unauthorized) }
+      it { expect(response.parsed_body['error']).to eq('You need to log in to continue!') }
+    end
+
+    context 'DELETE /tickets/1/comments' do
+      before { delete '/tickets/1/comments/1', as: :json }
+
+      it { expect(response).to have_http_status(:unauthorized) }
+      it { expect(response.parsed_body['error']).to eq('You need to log in to continue!') }
+    end
   end
 end
