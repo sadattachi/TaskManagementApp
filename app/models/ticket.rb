fbcec49f723ed1ab42697361ece2209221ecf7cd
sadattachi <nazarii200322@gmail.com>
2022-09-04 21:27:47 +0300

    Setup state machine

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,7 @@
 
 class Ticket < ApplicationRecord
   include ActiveModel::Dirty
+  include RailsStateMachine::Model
   # define_attribute_methods :title, :description
 
   belongs_to :worker
@@ -11,6 +12,38 @@ class Ticket < ApplicationRecord
 
   validates :title, length: { maximum: 40 }
   validates :worker_id, presence: true
-  validates :state, inclusion: { in: ['Pending', 'In progress', 'Done'],
-                                 message: '%<value>s is not a valid state' }
+
+  state_machine do
+    state :backlog, initial: true
+    state :pending
+    state :in_progress
+    state :waiting_for_accept
+    state :accepted
+    state :declined
+    state :done
+
+    event :get_from_backlog do
+      transitions from: :backlog, to: :pending
+    end
+
+    event :start_working do
+      transitions from: %i[pending declined], to: :in_progress
+    end
+
+    event :review do
+      transitions from: :in_progress, to: :waiting_for_accept
+    end
+
+    event :accept do
+      transitions from: :waiting_for_accept, to: :accepted
+    end
+
+    event :decline do
+      transitions from: :waiting_for_accept, to: :declined
+    end
+
+    event :finish do
+      transitions from: :accepted, to: :done
+    end
+  end
 end
