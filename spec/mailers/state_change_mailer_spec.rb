5a807e50de894404c5f70ec4cb2f11e28da239f1
sadattachi <nazarii200322@gmail.com>
2022-09-07 15:31:23 +0300

    Add change_state mailer specs

--- spec/mailers/state_change_mailer_spec.rb
+++ spec/mailers/state_change_mailer_spec.rb
@@ -3,5 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe StateChangeMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'notify_worker' do
+    let(:mail) do
+      described_class.with(ticket: Ticket.last).notify_worker
+    end
+
+    it { expect(mail.subject).to eq('Ticket state changed') }
+    it { expect(mail.to).to eq(['ted@gmail.com']) }
+    it { expect(mail.from).to eq(['test@example.com']) }
+
+    it { expect(mail.body.encoded).to match('Ticket "some" state changed to: done') }
+  end
+
+  describe 'notify_manager' do
+    let(:mail) do
+      described_class.with(ticket: Ticket.last).notify_manager
+    end
+
+    it { expect(mail.subject).to eq('Ticket state changed') }
+    it { expect(mail.to).to eq(['ivan@gmail.com']) }
+    it { expect(mail.from).to eq(['test@example.com']) }
+
+    it { expect(mail.body.encoded).to match('Ticket "some" is done') }
+  end
 end
